5ef12d7203c42d6851d75229471da3c2b7e3061e
Johansson <inste@icloud.com>
2019-01-27 21:50:56 +0200

    first commit

---
+++ TextPad/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TextPad
+//
+//  Created by Artem Karmaz on 2019-01-27.
+//  Copyright © 2019 Johansson Group. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+
+}
